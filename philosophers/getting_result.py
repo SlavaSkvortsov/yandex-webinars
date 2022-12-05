2682482b3717e7223dae0ed699ad346f83a17c0c
SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
2022-12-05 17:14:46 +0100

    Webinar 1: philosophers

---
+++ philosophers/getting_result.py
@@ -0,0 +1,24 @@
+import time
+from multiprocessing import
+
+
+def task() -> str:
+    print('Task started')
+    time.sleep(1)
+    print('Task finished')
+    return f'done {1}'
+
+
+def main() -> None:
+    with ProcessPoolExecutor() as executor:
+        # result = executor.map(task, range(5))
+        #
+        # for item in result:
+        #     print(item)
+        futures = [executor.submit(task) for _ in range(5)]
+        results = [future.result() for future in futures]
+        print(results)
+
+
+if __name__ == '__main__':
+    main()
---
+++ philosophers/philosophers_deadlock.py
@@ -0,0 +1,93 @@
+import threading
+import time
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+PHILOSOPHERS = 5
+TIMEOUT = 5
+WAIT = 0.5
+
+
+@dataclass
+class Fork:
+    lock: threading.Lock = field(default_factory=threading.Lock)
+
+    def acquire(self, blocking: bool = True) -> bool:
+        return self.lock.acquire(blocking=blocking)
+
+    def release(self) -> None:
+        if self.lock.locked():
+            self.lock.release()
+
+
+class Side(str, Enum):
+    LEFT = 'left'
+    RIGHT = 'right'
+
+
+@dataclass
+class Philosopher:
+    fork: dict[Side, Fork]
+    name: str
+    ate_count: int = 0
+
+    def run(self) -> None:
+        begin = time.monotonic()
+        while time.monotonic() - begin < TIMEOUT:
+            self._eat()
+            self._think()
+
+    def _eat(self) -> None:
+        self._acquire_fork(Side.LEFT)
+        self._acquire_fork(Side.RIGHT)
+
+        print(f'{self.name} is eating')
+        time.sleep(WAIT)
+        self.ate_count += 1
+        self._release_fork(Side.LEFT)
+        self._release_fork(Side.RIGHT)
+
+    def _acquire_fork(self, side: Side) -> bool:
+        print(f'{self.name} is trying to acquire {side} fork')
+        fork = self.fork[side]
+        time.sleep(WAIT)
+        return fork.acquire()
+
+    def _release_fork(self, side: Side) -> None:
+        fork = self.fork[side]
+        fork.release()
+
+    def _think(self) -> None:
+        print(f'{self.name} is thinking')
+        time.sleep(WAIT)
+
+
+def main() -> None:
+    forks = [Fork() for _ in range(PHILOSOPHERS)]
+    philosophers = [
+        Philosopher(
+            name=f'Philosopher {i}',
+            fork={
+                Side.LEFT: forks[i],
+                Side.RIGHT: forks[(i + 1) % 5],
+            },
+        ) for i in range(5)
+    ]
+
+    threads = [threading.Thread(target=philosopher.run) for philosopher in philosophers]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.stop = True
+        thread.join()
+
+    print('Philosophers ate:')
+    for philosopher in philosophers:
+        print(f'{philosopher.name}: {philosopher.ate_count}')
+
+
+if __name__ == '__main__':
+    main()
